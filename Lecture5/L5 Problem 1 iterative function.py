1c8c4498cfde1c7d9cdf3c80f5e293d90d808d71
khadak-bogati <61652776+khadak-bogati@users.noreply.github.com>
2020-06-16 19:12:40 -0500

    Create L5 Problem 1 iterative function.py

---
+++ Lecture5/L5 Problem 1 iterative function.py
@@ -0,0 +1,17 @@
+# Write an iterative function iterPower(base, exp) 
+# that calculates the exponential baseexp by simply using successive multiplication.
+
+def iterPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+ 
+    returns: int or float, base^exp
+    '''
+
+    result = 1
+    while exp>0:
+        result = base * result
+        exp = exp - 1
+    return result
+
